7a2a4e6972c5a2f38dc812d76e585bfce63fe1d0
fedor <parashchenko.fedor@gmail.com>
2024-03-08 00:16:22 +0300

    working on auth, start with sign-up page frontend

---
+++ gui/src/components/auth/Auth.tsx
@@ -0,0 +1,71 @@
+import {Link} from "react-router-dom";
+import {useState} from "react";
+import axios from "axios";
+import {useNavigate} from "react-router-dom";
+
+export default function Auth(){
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const navigate = useNavigate();
+
+    const handleSubmit = async (event: { preventDefault: () => void; }) => {
+        event.preventDefault();
+        try {
+            console.log(password)
+            const response = await axios.post('http://localhost:5000/login', { //TODO: переделать в env
+                email: email,
+                password: password
+            }, {
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                withCredentials: true
+            });
+
+            if (response.status == 200) {
+                const {token} = response.data
+                //console.log(token)
+                localStorage.setItem("SavedToken", token);
+                // Установка JWT в заголовки axios для последующих запросов
+                axios.defaults.headers.common['Authorization'] = 'Bearer ' + token;
+                navigate('/')
+            }
+
+        } catch (error) {
+            console.error(error);
+        }
+
+        console.log(`Username: ${email}, Password: ${password}`);
+    };
+
+    return (
+        <>
+            <div>Start page</div>
+            <Link to="/home">
+                <text>Visit your profile</text>
+            </Link>
+
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="username">Username:</label>
+                <input
+                    type="text"
+                    id="username"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                <br/>
+                <label htmlFor="password">Password:</label>
+                <input
+                    type="password"
+                    id="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                <br/>
+                <input type="submit" value="Submit"/>
+            </form>
+        </>
+    )
+}
